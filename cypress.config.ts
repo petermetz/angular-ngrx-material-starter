7a436c92be5dc8457c0651aae34267a9078e663c
Peter Somogyvari <peter.metz@unarin.com>
2024-05-27 14:41:29 -0700

    test(e2e): migrated to Cypress for end to end testing - WORKS

---
+++ cypress.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    specPattern: ['projects/angular-ngrx-material-starter/e2e/src/**/*.cy.ts']
+  }
+});
