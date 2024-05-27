c62578300fb1cec1faccc1e4bdba133af08de30e
Peter Somogyvari <peter.metz@unarin.com>
2024-05-27 15:35:24 -0700

    test: migrated e2e/src/app/app.e2e-spec.ts to Cypress as well

--- projects/angular-ngrx-material-starter/e2e/src/features/about/about.e2e-spec.cy.ts
+++ projects/angular-ngrx-material-starter/e2e/src/features/about/about.e2e-spec.cy.ts
@@ -9,12 +9,10 @@ describe('About Page', () => {
 
   it('should display "Get started" H2', () => {
     cy.get('h2:contains("Get started")');
-    // cy.get('section#getting-started').should('be.visible'); // Check if "Getting Started" section is visible
   });
 
   it('first action button should lead to "Features" route', () => {
     cy.get('button:contains("Features")').click();
-    // cy.get('button').eq(0).click(); // Click the first action button
     cy.url().should('include', '/feature-list'); // Check the URL to ensure it navigated to the "Features" route
   });
 });
